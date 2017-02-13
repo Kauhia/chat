81d8e43124bf36de5b19f23f7592e2282ab21df2
Ek <e.kouhia@gmail.com>
2017-02-13 21:14:35 +0200

    fix layout

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var socket = io();
+socket.on('message', function(data) {
+  console.log('received message: ', data.message);
+  messagesBox.appendMessage(data.message);
+})
+
+
 Vue.component('message-item', {
   props: ['message'],
   template: '<p>{{ message.text }}</p>'
@@ -17,36 +24,3 @@ var app = new Vue({
   },
   methods:{}
 })
-
-/*
-  var messagesBox = new Vue({
-    el: '#messagesBox',
-      data: {
-        messages: ['Hello', 'Hi', 'Howdy']
-      },
-      methods: {
-        appendMessage: function(msg) {
-          this.$data.messages.push(msg)
-        }
-      }
-    })
-
-  var socket = io();
-  socket.on('message', function(data) {
-    console.log('received message: ', data.message);
-    messagesBox.appendMessage(data.message);
-  })
-
-  var sendBox = new Vue({
-    el: '#sendBox',
-    data: {
-      message:""
-    },
-    methods: {
-      sendMessage: function() {
-        var msg = this.$data.message;
-        messagesBox.appendMessage(msg);
-        socket.emit('client message', msg);
-      }
-    }
-  })*/
